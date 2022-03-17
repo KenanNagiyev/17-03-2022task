c3cae8e31f2bd33f5d61ffbd15dbb35fdc3dc150
KenanNagiyev <788lnmp@code.edu.az>
2022-03-17 11:26:42 +0400

    new file

---
+++ acces_modifies/Models/Student.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ClassLibrary1;
+
+namespace acces_modifies.Models
+{
+    public class Student:Person
+    {
+        public void Semi()
+        {
+            Age = 15;
+            Person a = new Person();
+            
+        }
+    }
+}
---
+++ acces_modifies/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using ClassLibrary1;
+namespace acces_modifies
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person p = new Person();
+            Teacher t = new Teacher();
+            
+        }
+    }
+}
